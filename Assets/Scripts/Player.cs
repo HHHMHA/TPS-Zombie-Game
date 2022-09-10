e24a0469664d6d3a70bccd30c09ebea48e46dd38
J2mF <mp4-12cs5@hotmail.com>
2022-09-10 10:59:21 +0300

    Move in camera direction

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
     [Header( "Player Movement" )] [SerializeField] private float playerSpeed = 1.9f;
+
+    [Header( "Player Script Cameras" )] [SerializeField]
+    private Transform playerCamera;
     
     [Header( "Player Animator and Gravity" )] [SerializeField]
     private CharacterController controller;
@@ -25,10 +28,12 @@ public class Player : MonoBehaviour {
         Vector3 direction = new Vector3( horizontalAxis, 0f, verticalAxis ).normalized;
 
         if ( direction.magnitude >= 0.1f ) {
-            float targetAngle = Mathf.Atan2( direction.x, direction.z ) * Mathf.Rad2Deg;
+            float targetAngle = Mathf.Atan2( direction.x, direction.z ) * Mathf.Rad2Deg + playerCamera.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle( transform.eulerAngles.y, targetAngle, ref turnCalmVelocity, turnCalmTime );
             transform.rotation = Quaternion.Euler( 0f, angle, 0f );
-            controller.Move( direction.normalized * (playerSpeed * Time.deltaTime) );
+
+            Vector3 moveDirection = Quaternion.Euler( 0f, targetAngle, 0f ) * Vector3.forward;
+            controller.Move( moveDirection.normalized * (playerSpeed * Time.deltaTime) );
         }
     }
 }
