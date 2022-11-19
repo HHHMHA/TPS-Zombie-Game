4fdc95134abe75a111cc222b07b338ca094bdfb5
J2mF <mp4-12cs5@hotmail.com>
2022-11-19 11:36:42 +0300

    Add raycast shooting

---
+++ Assets/Scripts/Rifle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+public class Rifle : MonoBehaviour {
+    [Header( "Rifle Things" )] [SerializeField]
+    private Camera camera;
+    [SerializeField] private float giveDamageOf = 10f;
+    [SerializeField] private float shootingRange = 100f;
+
+    private void Update() {
+        if ( Input.GetButtonDown( "Fire1" ) ) {
+            Shoot();
+        }
+    }
+
+    private void Shoot() {
+        RaycastHit hit;
+
+        var transform1 = camera.transform;
+        if ( Physics.Raycast( transform1.position, transform1.forward, out hit, shootingRange ) ) {
+            
+        }
+    }
+}
