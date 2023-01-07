9693f4161b3aaa42f7c4ee32913d1aec161125d2
J2mF <mp4-12cs5@hotmail.com>
2023-01-07 14:16:19 +0300

    feat: implement fire rate for player rifle

--- Assets/Scripts/Rifle.cs
+++ Assets/Scripts/Rifle.cs
@@ -6,14 +6,16 @@ public class Rifle : MonoBehaviour {
     private Camera camera;
     [SerializeField] private float giveDamageOf = 10f;
     [SerializeField] private float shootingRange = 100f;
+    [SerializeField] private float fireCharge = 15;
+    private float nextTimeToShoot = 0f;
 
     [Header( "Rifle Effects" )] [SerializeField]
     private ParticleSystem muzzleSpark;
 
     private void Update() {
-        if ( Input.GetButtonDown( "Fire1" ) ) {
-            Shoot();
-        }
+        if ( !Input.GetButton( "Fire1" ) || !( Time.time >= nextTimeToShoot ) ) return;
+        nextTimeToShoot = Time.time + 1f / fireCharge;
+        Shoot();
     }
 
     private void Shoot() {
