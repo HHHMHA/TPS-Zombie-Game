73737b4736a9b0f9a9f773ee328516379861a4fa
J2mF <mp4-12cs5@hotmail.com>
2023-02-11 21:26:54 +0300

    feat: implement rifle reload and ammo

--- Assets/Scripts/Rifle.cs
+++ Assets/Scripts/Rifle.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using UnityEditor.Build;
 using UnityEngine;
 
 public class Rifle : MonoBehaviour {
@@ -8,17 +10,53 @@ public class Rifle : MonoBehaviour {
     [SerializeField] private float shootingRange = 100f;
     [SerializeField] private float fireCharge = 15;
     private float nextTimeToShoot = 0f;
+    public Player player;
 
+    [Header( "Rifle Ammunition and shooting" )]
+    [SerializeField] private int maximumAmmunition = 32;
+    [SerializeField] private int mag = 10;
+    [SerializeField] private int presentAmmunition;
+    [SerializeField] private float reloadingTime = 1.3f;
+    private bool setReloading = false;
+    
+    
     [Header( "Rifle Effects" )] [SerializeField]
     private ParticleSystem muzzleSpark;
 
+
+    private void Awake() {
+        presentAmmunition = maximumAmmunition;
+        
+    }
     private void Update() {
+        if (setReloading)
+            return;
+
+        if ( presentAmmunition <= 0 ) {
+            StartCoroutine( Reload() );
+            return;
+        }
+        
         if ( !Input.GetButton( "Fire1" ) || !( Time.time >= nextTimeToShoot ) ) return;
         nextTimeToShoot = Time.time + 1f / fireCharge;
         Shoot();
     }
 
     private void Shoot() {
+        // check for mag
+        if ( mag == 0 ) {
+            // show ammo out text
+            return;
+        }
+
+        presentAmmunition--;
+
+        if ( presentAmmunition == 0 ) {
+            mag--;
+        }
+        // updating the UI
+        
+        
         muzzleSpark.Play();
         RaycastHit hit;
 
@@ -29,4 +67,17 @@ public class Rifle : MonoBehaviour {
             objectToHit.Damage( giveDamageOf, hit.point, Quaternion.LookRotation( hit.normal ) );
         }
     }
+
+    IEnumerator Reload() {
+        setReloading = true;
+        Debug.Log( "Reloading ..." );
+        // play anim
+        // play reload sound
+        yield return new WaitForSeconds( reloadingTime );
+        // play anim
+        presentAmmunition = maximumAmmunition;
+        setReloading = false;
+
+    }
+    
 }
